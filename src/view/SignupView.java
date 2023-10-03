389595962f5a18fa04f7f13dc547575639ce1ea5
Alec Dong <alec.dong@mail.utoronto.ca>
2023-10-03 14:25:42 -0400

    lab activity done

--- src/view/SignupView.java
+++ src/view/SignupView.java
@@ -52,9 +52,10 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
                 new ActionListener() {
                     public void actionPerformed(ActionEvent evt) {
                         if (evt.getSource().equals(signUp)) {
-                            signupController.execute(usernameInputField.getText(),
-                                    String.valueOf(passwordInputField.getPassword()),
-                                    String.valueOf(repeatPasswordInputField.getPassword()));
+                            SignupState currState = signupViewModel.getState();
+                            signupController.execute(currState.getUsername(),
+                                    currState.getPassword(),
+                                    currState.getRepeatPassword());
                         }
                     }
                 }
@@ -82,6 +83,40 @@ public class SignupView extends JPanel implements ActionListener, PropertyChange
                     public void keyReleased(KeyEvent e) {
                     }
                 });
+
+        passwordInputField.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        SignupState currState = signupViewModel.getState();
+                        currState.setPassword(String.valueOf(passwordInputField.getPassword()) + e.getKeyChar());
+                        signupViewModel.setState(currState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {}
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {}
+                }
+        );
+
+        repeatPasswordInputField.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        SignupState currState = signupViewModel.getState();
+                        currState.setRepeatPassword(String.valueOf(repeatPasswordInputField.getPassword()) + e.getKeyChar());
+                        signupViewModel.setState(currState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {}
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {}
+                }
+        );
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         this.add(title);
